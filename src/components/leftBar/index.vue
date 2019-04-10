d11b6492949e4ced4d5564c7726dc5d6ddc0b4bf
琦玉 <qiyu@tongbanjie.com>
2019-04-10 09:57:20 +0800

    修改left-bar

--- src/components/leftBar/index.vue
+++ src/components/leftBar/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="left-bar">
+  <div class="left-bar dark-menu">
     <div class="title">
       <i class="logo"></i>
       <span>Vue-SW</span>
@@ -11,8 +11,7 @@
     >
       <el-submenu index="1">
         <template slot="title">
-          <ac-icon icon="appstore-fill" :size="16"/>
-          <span>配置中心</span>
+          <ac-icon icon="appstore-fill" :size="15"/><span>配置中心</span>
         </template>
         <el-menu-item index="/home">首页</el-menu-item>
         <el-menu-item index="/activity">活动</el-menu-item>
@@ -28,12 +27,12 @@
     width 200px
     height 100vh
     box-shadow 1px 0px 1px rgba(20,20,20,.1)
-    z-index 2
+    z-index 1
     .title
       font-size 18px
-      font-weight 600
-      color #409eff // #409eff
-      padding 0 18px
+      font-weight 500
+      color #FFF // #409eff
+      padding 0 20px
       line-height 53px
       margin-bottom 5px
       box-shadow 0 1px 9px -3px rgba(0,0,0,.2)
@@ -45,35 +44,50 @@
         margin-right 3px
         background url('../../assets/logo.png') no-repeat center
         background-size contain
-      span
-        vertical-align middle
-     .el-menu
-      border-right none
+
+    .el-menu
+      border none
       width 200px
-      .el-submenu .el-menu-item
-        border-right 2px solid #FFF
-        min-width 180px
+      .el-menu-item, .el-submenu__title
+        height 48px
+        font-size 13px
+        color #505153
+      .el-submenu
+        line-height 48px
+      .el-submenu__title
+        line-height 47px
+        .ac-icon
+          margin-right 5px
+
+  .dark-menu
+    background-color #001529
+    .el-menu
+      background-color #001529
+      .el-menu-item, .el-submenu__title
+        color rgba(255,255,255,.7)
+        &:hover
+          color #FFF
+          background-color transparent
+          i
+            color #FFF
         &.is-active
-          border-color #409EFF
-          background-color rgba(0,121,255,.08)
-    .el-submenu__title
-      color #505153
-      span
-        margin-left 3px
-    .el-menu-item
-      color #505153
+          background-color #1890ff
+          color #FFF
+        i
+          color rgba(255,255,255,.7)
+
+      .el-submenu .el-menu
+        background-color: #000c17;
+        box-shadow: inset 0 2px 8px rgba(0,0,0,.45)
 
   /* important */   
-  @media screen and (max-width: 1367px)
-    .left-bar
-      width 185px
-      .el-menu
-         width 185px
-      .el-submenu__title
-        height 50px
-        line-height 50px
-      .el-submenu .el-menu-item
-        height 45px
-        line-height 45px
+  // @media screen and (max-width: 1367px)
+  //   .left-bar
+  //     .el-submenu__title
+  //       height 45px
+  //       line-height 44px
+  //     .el-submenu .el-menu-item
+  //       height 45px
+  //       line-height 45px
         
 </style>
